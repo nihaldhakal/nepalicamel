22d1026259e31fe33a61c25a786d826d69cc5068
Nihaal Dhakal <nihaldhakal41@gmail.com>
2018-01-17 21:48:59 +0545

    Added mailcatcher in dev env.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -52,6 +52,10 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   # need for devise
+
+  # mailcatcher
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.raise_delivery_errors = false
 
 end
